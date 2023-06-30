3cbd6dbf643ffa3909f5584d6608895dd35a18a6
litemon11 <138236471+litemon11@users.noreply.github.com>
2023-06-30 22:05:20 +0530

    Add files via upload

---
+++ Inheritance1.java
@@ -0,0 +1,29 @@
+/*Page 465; 26 Program 12.9
+A super class Worker has been defined to store the details of a worker. Define a subclass Wages to compute
+the monthly wages for the worker. The details/specifications of bouth the classe are given below.
+
+    Class Name       :                            Worker
+Data Members/instance variables:
+    Name             :    to store the name of the worker
+    Basic            :    to store the basic pay in decimals
+Member functions:
+    Worker(...)      :    Parameterised constructor to assign values to the instance variables
+    void display()   :    display the workers details
+
+    Class name       :                            Wages
+Data Members/instance variables:
+    hrs              :    stores the hourse worked
+    rate             :    stores rate per hour
+    wages            :    stores the overall wage of the worker
+Member functions:
+    Wages(...)       :    Parameterised constructor to assign values to the instance variables of both
+                          the classes
+    double overtime():    Calculates and returns the overtime amount as (hours*rate)
+    void display.    :    Calculates the wage using the formula wage = overtime amount + Basic pay
+                          and display it along with the other details
+
+Specify the class Worker giving details of the constructor() and void display(). Using the concept of
+inheritance specify the class Wages giving details of constructor(), double overtime() and void display().
+The main() function need not be written.
+*/
+
